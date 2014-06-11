e19dc6844710ecadd6b76bf5a92be552049e816f
Administrator <fabrizio.balliano@gmail.com>
2014-06-11 23:05:46 +0200

    first completely working version

--- shell/fballiano_full_catalog_translate.php
+++ shell/fballiano_full_catalog_translate.php
@@ -43,8 +43,9 @@ class Fballiano_FullCatalogTranslate_Shell extends Mage_Shell_Abstract
         $this->ws_url .= "?key={$this->api_key}";
 
         $args = array_keys($this->_args);
-        $this->store_source = $args[0];
-        $this->store_dest = $args[1];
+        $this->store_source = @$args[0];
+        $this->store_dest = @$args[1];
+        if (!$this->store_source or !$this->store_dest) die($this->usageHelp());
 
         require_once "Magmi/inc/magmi_defs.php";
         require_once "Magmi/integration/inc/magmi_datapump.php";
@@ -55,26 +56,51 @@ class Fballiano_FullCatalogTranslate_Shell extends Mage_Shell_Abstract
 
         $appEmulation = Mage::getSingleton("core/app_emulation");
 
-        $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($this->store_dest);
+        try {
+            $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($this->store_dest);
+        } catch (Mage_Core_Model_Store_Exception $e) {
+            die("Target store view \"{$this->store_dest}\" doesn't seem to exist\n" . $this->usageHelp());
+        }
+
+        $store_id_dest = Mage::app()->getStore()->getId();
         $this->language_dest = substr(Mage::app()->getLocale()->getLocaleCode(), 0, 2);
         $this->ws_url .= "&target={$this->language_dest}";
+
         $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
 
-        $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($this->store_source);
+        $attribute_id = Mage::getModel("catalog/entity_attribute")->loadByCode(Mage_Catalog_Model_Product::ENTITY, "fb_translate")->getId();
+        $products = Mage::getSingleton("core/resource")->getConnection("core_read")->fetchCol("SELECT entity_id FROM catalog_product_entity_int WHERE attribute_id={$attribute_id} AND store_id={$store_id_dest} AND value=1");
+
+        try {
+            $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation($this->store_source);
+        } catch (Mage_Core_Model_Store_Exception $e) {
+            die("Source store \"{$this->store_source}\" view doesn't seem to exist\n" . $this->usageHelp());
+        }
+
         $this->language_source = substr(Mage::app()->getLocale()->getLocaleCode(), 0, 2);
         $this->ws_url .= "&source={$this->language_source}";
 
-        $products = Mage::getResourceModel("catalog/product_collection")
-            ->addStoreFilter()
-            ->addAttributeToFilter("fb_translate", "1");
-        foreach ($this->attributes_to_translate as $attribute) {
-            $products->addAttributeToFilter($attribute, array('like' => '%'));
-        }
+        $product = Mage::getModel("catalog/product");
+        foreach ($products as $product_id) {
+            $product->load($product_id);
+            $row = $product->getData();
 
-        Mage::getSingleton('core/resource_iterator')->walk(
-            $products->getSelect(),
-            array(array($this, 'productCollectionWalkCallback'))
-        );
+            echo "Translating {$row["sku"]} from {$this->language_source} to {$this->language_dest}... ";
+            $translated_row = array();
+            $translated_row["store"] = (string)$this->store_dest;
+            $translated_row["sku"] = (string)$row["sku"];
+            $translated_row["fb_translate"] = "0"; //leave it as string otherwise magmi won't save it
+            foreach ($this->attributes_to_translate as $attribute) {
+                if (strlen($row[$attribute])) {
+                    $ws_url = "{$this->ws_url}&q=" . urlencode($row[$attribute]);
+                    $translated = json_decode(file_get_contents($ws_url), true);
+                    $translated = $translated["data"]["translations"][0]["translatedText"];
+                    $translated_row[$attribute] = (string)$translated;
+                }
+            }
+            $this->datapump->ingest($translated_row);
+            echo "OK\n";
+        }
 
         $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
         $this->datapump->endImportSession();
@@ -100,6 +126,15 @@ class Fballiano_FullCatalogTranslate_Shell extends Mage_Shell_Abstract
 
         $this->datapump->ingest($translated_row);
         echo "OK\n";
+    }
+
+    public function usageHelp()
+    {
+        return <<<USAGE
+
+Usage:  php -f fballiano_full_catalog_translate.php source_store_view_code target_store_view_code
+
+USAGE;
     }
 }
 
